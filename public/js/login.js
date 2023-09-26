f69ced734394e68932f5a18bf628e28e4661abbf
Elizabeth Kovalchick <ekovalchick97@gmail.com>
2023-09-26 19:35:39 -0400

    added handlebars js

--- public/js/login.js
+++ public/js/login.js
@@ -1,3 +1,62 @@
+//js for format
+
+//3 Scripts: SMOOTH SCROLL, GOOGLE MAPS, CHANGE CLASS
+
+//SMOOTH SCROLLING
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+
+//CHANGE CSS CLASS IN HTML
+	$(document).scroll(function() {
+	var swap_class = document.getElementById("change_class");
+
+	   if($(window).scrollTop() === 0) {
+	     swap_class.classList.remove("menu_color_change");
+	   } else {
+	   		swap_class.classList.add("menu_color_change");
+	   	}
+	});
+
+//end js for format
+
+
+
+
+
 const loginFormHandler = async (event) => {
   event.preventDefault();
 
