1fa06dd618e54c549b4149d05d87b26164f70e8c
Elizabeth Kovalchick <ekovalchick97@gmail.com>
2023-10-02 16:03:12 -0400

    update html for surboard page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./views/layouts/main.handlebars"],
+  content: ["./views/layouts/main.handlebars*.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
